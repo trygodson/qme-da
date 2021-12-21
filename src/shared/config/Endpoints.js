c43de870cac17cddb94cfafd975e2dfe6ab7e1e9
trygodson <gnqnpn@gmail.com>
2021-12-21 10:29:50 +0100

    http to https

--- src/shared/config/Endpoints.js
+++ src/shared/config/Endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://shop.scnip.net/api/v1/';
+const BASE_URL = 'https://shop.scnip.net/api/v1/';
 // const BASE_URL = 'http://localhost:8000/api/v1/';
 const ApiEndpoints = {
   //Doctor
